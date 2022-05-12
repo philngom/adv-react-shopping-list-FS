e51b6694d0a59eb33402785422706356ce6abb38
Philippe Ngom <philngom@gmail.com>
2022-05-11 18:24:33 -0700

    create context

---
+++ src/context/ShoppingListProvider.jsx
@@ -0,0 +1,49 @@
+import { createContext, useContext, useReducer } from 'react';
+
+const initialState = [];
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case 'add':
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          item: action.payload.item,
+          done: false,
+        },
+      ];
+    default:
+      throw new Error('In reducer function');
+  }
+};
+
+const ShoppingListContext = createContext();
+
+export const ShoppingListProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addItem = (e) => {
+    e.preventDefault();
+    dispatch({ type: 'add', payload: { item } });
+    setItem('');
+  };
+
+  return (
+    <ShoppingListContext.Provider value={addItem}>
+      {children}
+    </ShoppingListContext.Provider>
+  );
+};
+
+export const useShoppingList = () => {
+  const context = useContext(ShoppingListContext);
+
+  if (!context)
+    throw new Error(
+      'useShoppingList must be called from within ShoppingListProvider'
+    );
+
+  return context;
+};
